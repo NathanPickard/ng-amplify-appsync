6522e22b4ca2e305fcf373c49ec259c31b1ccac5
Nathan Pickard <nathan.pickard87@gmail.com>
2019-10-12 14:54:17 -0700

    Publishing graphql API to AWS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { API, graphqlOperation } from '../graphql/queries';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listRestaurants } from '../graphql/queries';
+// import { Restaurant } from '../API';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-amplify-appsync';
+export class AppComponent implements OnInit {
+  restaurants: Array<Restaurant>;
+
+  async ngOnInit() {
+    var response = await
+      API.graphql(graphqlOperation(listRestaurants))
+    this.restaurants = (response as any).data.listRestaurants.items;
+  }
 }
